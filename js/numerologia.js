f537664260f152f02c2bf1ee49b6796ff40ccf5f
Felipe Velayos <Felipe.velayos@ricoh.es>
2018-02-05 18:19:48 +0100

    corregido número de selección en sesión

--- js/numerologia.js
+++ js/numerologia.js
@@ -219,9 +219,7 @@ window.app.load = function() {
     navigateQueryString();
   });
   $("#containernum div.alert").click(function() {
-    if (typeof dataObj.num === "undefined" || isNaN(parseInt(null))) {
-      qsObj.num = $(this).text();
-    }
+    qsObj.num = $(this).text();
     qsObj.pag = 4;
     navigateQueryString();
   });
